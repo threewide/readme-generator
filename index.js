1da066809aa5eb82317aadd9c9d5b8b5a9a9c0c6
Justin Collver <collver_99@hotmail.com>
2022-12-04 10:44:58 -0500

    setup project and install packages

---
+++ index.js
@@ -0,0 +1,16 @@
+// install inquirer, filestream, wgit 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const writeFile = require ('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
